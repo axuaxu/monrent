d01305f2a9c3dc98fcc9b12e22b47b7f0d80b886
Anne <axu@yahoo.com>
2016-10-09 10:59:30 -0400

    3

--- rent/admin.py
+++ rent/admin.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
 
+from .models import Monrent,Plinks
+
 # Register your models here.
+
+class PlinksAdmin(admin.ModelAdmin):
+    fields = ['link']
+
+class MonrentAdmin(admin.ModelAdmin):
+    fields = ['title','address']
+
+admin.site.register(Plinks,PlinksAdmin)
+admin.site.register(Monrent,MonrentAdmin)
+
+

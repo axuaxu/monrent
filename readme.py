88624d2fdf8fdbce98e25472b26161403c1551cd
Anne <axu@yahoo.com>
2016-10-09 20:11:23 -0400

    4

---
+++ readme.py
@@ -0,0 +1,27 @@
+
+version 2  
+
+monrent.py  
+    click through links and pagenation   selenium
+    save detailed info table monrent
+
+django project monrent
+ 
+verion 1
+
+db_create.py
+    table structure
+
+xpath.py 
+   page element xpath
+
+savepages.py
+    save html locally iu91.com/content/listRS?page=
+savejs.py
+    save javascripts locally on iu91.com/re/rent 
+
+plinks.py
+    save links on iu91.com/content/listRS?page= to table plinks  montreal.sqlite
+
+rentpage.py
+    save pages in plinks to table rentpage   
--- rent/admin.py
+++ rent/admin.py
@@ -10,9 +10,16 @@ class PlinksAdmin(admin.ModelAdmin):
     fields = ['link']
 
 class MonrentAdmin(admin.ModelAdmin):
-    fields = ['title','address']
+    #fields = ['title','address']
+    list_display = ('num','udate','title','address','pcode','rent','rooms','rstyle','intime','method','length')
 
+class MonrentProxy(Monrent):
+    class Meta:
+        proxy = True
+class MonrentProxyAdmin(admin.ModelAdmin):
+    list_display = ('num','udate','title','pcode','rooms','rent') 
+    list_filter = ('pcode','rooms')
 admin.site.register(Plinks,PlinksAdmin)
 admin.site.register(Monrent,MonrentAdmin)
-
+admin.site.register(MonrentProxy,MonrentProxyAdmin)
 

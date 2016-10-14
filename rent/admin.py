c6789d825031c06a68299c8516f4f71deba112b3
Anne <axu@yahoo.com>
2016-10-14 18:59:29 -0400

    30

--- rent/admin.py
+++ rent/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 
-from .models import Monrent,Plinks
+from .models import Monrent,Plinks,Monrent_flex_all
 
 # Register your models here.
 
@@ -17,9 +17,16 @@ class MonrentProxy(Monrent):
     class Meta:
         proxy = True
 class MonrentProxyAdmin(admin.ModelAdmin):
-    list_display = ('num','udate','title','pcode','rooms','rent') 
+    list_display = ('num','udate','title','pcode','rooms','rent','surl') 
+    list_filter = ('method','length','rooms','pcode')
+class Monrent_flex_allAdmin(admin.ModelAdmin):
+    list_display = ('num','udate','title','pcode','rooms','rent')
     list_filter = ('pcode','rooms')
+
+
+
 admin.site.register(Plinks,PlinksAdmin)
 admin.site.register(Monrent,MonrentAdmin)
 admin.site.register(MonrentProxy,MonrentProxyAdmin)
+admin.site.register(Monrent_flex_all,Monrent_flex_allAdmin)
 

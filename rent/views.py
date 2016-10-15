2cc1b5a1b521b16b80d0bfcc07610c5e333fefc9
Anne <axu@yahoo.com>
2016-10-14 23:11:43 -0400

    5

--- rent/views.py
+++ rent/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from  .models import Monrent
 # Create your views here.
 
 def index(request):
     rent_list = Monrent.objects.order_by('id')
-    output = ','.join([r.title for r in rent_list])
-    template = loader.get_template('rent/index.html')
+    #output = ','.join([r.title for r in rent_list])
+    #template = loader.get_template('rent/index.html')
     context = {'properties for rent': rent_list,
               }
     #return HttpResponse("hello, proj monrent rent app")
-    return HttpResponse(template.render(context,request))
-
+    #return HttpResponse(template.render(context,request))
+    return render(request,'rent/index.html',context)
 def detail(request,monrent_id):
     return HttpReponse("This is the detail page%s." % monrent_id)
